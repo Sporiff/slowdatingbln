5ccd4be9ff660f3c4f0beec9b83ffa7e6cefa3c2
Ciarán Ainsworth <cda@sporiff.dev>
2024-07-24 15:49:29 +0200

    Improved design

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,8 +3,8 @@
 interface ImportMetaEnv {
   readonly MAILGUN_KEY: string;
   readonly MAILGUN_ENDPOINT: string;
-  readonly EMAIL: string;
-  readonly GRAPHQL_ENDPOINT: string;
+  readonly PUBLIC_EMAIL: string;
+  readonly PUBLIC_GRAPHQL_ENDPOINT: string;
 }
 
 interface ImportMeta {
