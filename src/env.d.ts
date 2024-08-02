42f65b951c1fa02fc77413ba12a9801b8c1266fc
Ciarán Ainsworth <cda@sporiff.dev>
2024-08-02 15:28:46 +0200

    Use Hubspot for email form

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,6 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly MAILGUN_KEY: string;
-  readonly MAILGUN_ENDPOINT: string;
-  readonly PUBLIC_EMAIL: string;
   readonly PUBLIC_GRAPHQL_ENDPOINT: string;
 }
 
