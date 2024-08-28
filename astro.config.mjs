3a4b5be23dd3ee6720b103b12a92815fc93cfd00
Ciarán Ainsworth <cda@sporiff.dev>
2024-08-28 23:45:04 +0200

    Add site variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://slowdatingbln.com",
   integrations: [
     tailwind({
       applyBaseStyles: false,
